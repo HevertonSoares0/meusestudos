502aba047566da8af0816c569dc590c8a35fc421
unknown <heverton.soares0@gmail.com>
2023-03-13 01:47:39 -0300

    testecommit

---
+++ desafios_bee/1019_conversaotempo - teste.js
@@ -0,0 +1,20 @@
+/*
+Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+Entrada
+
+O arquivo de entrada contém um valor inteiro N.
+Saída
+
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+1h = 3600
+*/
+
+const input = require("readline-sync");
+
+let segundos = parseInt(input.question("Segundos "));
+
+let minutos = segundos*60;
+console.log(minutos);
+
+let horas = minutos/60;
+console.log(horas);
